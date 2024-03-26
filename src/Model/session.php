af05c1a39d9025f370e45858d59aba053026845f
ahammiche <adelhammiche.pro@gmail.com>
2024-03-26 11:27:49 +0100

    get Session

--- src/Model/session.php
+++ src/Model/session.php
@@ -9,6 +9,15 @@ class SessionFunc
     public function doSessionExist(){
         return empty($_SESSION["id_user"]) ;
     }
+
+    public function createSession($user){
+        session_start();
+	    $_SESSION["id"] = $user;
+    }
+
+    public function getSession(){
+        return $_SESSION["id"];
+    }
     
 }
 ?>
