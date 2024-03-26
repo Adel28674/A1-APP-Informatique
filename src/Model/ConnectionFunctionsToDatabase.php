ec947907e1233ef2ae7c5b5651471b6ff3e4cfde
ahammiche <adelhammiche.pro@gmail.com>
2024-03-26 10:29:05 +0100

    Creation of the database Methods

---
+++ src/Model/ConnectionFunctionsToDatabase.php
@@ -0,0 +1,33 @@
+<?php
+
+class ConnectionFunctions
+{
+
+    public function db_Connect($servor, $uname, $password, $db_name){
+        return mysqli_connect($servor, $uname, $password, $db_name);
+    }
+
+    public function areFieldsFilled($username, $password)
+    {
+        if (empty($username) || empty($password)) { // Champ(s) vide(s)
+            return false;
+        } else {
+            return true;
+        } // Champs remplis
+    }
+
+    public function connexionDatabaseReussie($connexion)
+    {
+        if ($connexion->connect_error) { // Erreur de connexion
+            return false;
+        } else {
+            return true;
+        } // Connexion réussie
+    }
+
+    public function utilisateurInscrit($state){
+        if ($state->num_rows == 1) {
+            return true;
+        } else {return false;}
+    }
+}
